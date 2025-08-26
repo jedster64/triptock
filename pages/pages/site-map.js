5b35a80f79346bc69acd0274f905b347b061acf0
jedster64 <jedster64@gmail.com>
2025-08-26 19:29:01 +0100

    Create site-map.js

---
+++ pages/pages/site-map.js
@@ -0,0 +1,13 @@
+export default function SiteMap() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>Site Map</h1>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/listings">Listings</a></li>
+        <li><a href="/about">About</a></li>
+        <li><a href="/site-map">Site Map</a></li>
+      </ul>
+    </main>
+  );
+}

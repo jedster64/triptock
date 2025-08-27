51f55c40edb5173fe7006c25c8b5770eb92413cc
jedster64 <jedster64@gmail.com>
2025-08-27 12:21:02 +0100

    chore: fix next.config.js, add .gitignore, successful build

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD
-const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
-=======
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
 };
 
 module.exports = nextConfig;
->>>>>>> 3f1e434cb69875717587b4c04836e0f6c6be4dba
